438060430f7d86c308ba8da89a721f8089f7b7cc
shus521 <1780848749@qq.com>
2018-11-14 17:21:29 +0800

    20181114-新增人民币转换接口

--- src/main/java/com/tyss/web/CommonController.java
+++ src/main/java/com/tyss/web/CommonController.java
@@ -7,6 +7,9 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Arrays;
+import java.util.List;
+
 //一些小工具
 @RestController
 @RequestMapping(value = "/common")
@@ -42,6 +45,33 @@ public class CommonController {
             commonResponseBean.setCode(-2);
             commonResponseBean.setMsg("转换错误");
         }
+        return commonResponseBean;
+    }
+
+    /**
+     * 数字转人民币
+     */
+    @RequestMapping(value = "/getMoney")
+    public CommonResponseBean getRMB(@RequestParam(value = "money") String money) {
+        CommonResponseBean commonResponseBean = new CommonResponseBean();
+        if (money.length() > 9 || money.length() < 1) {
+            commonResponseBean.setCode(-1);
+            commonResponseBean.setMsg("金额超限");
+            return commonResponseBean;
+        }
+        int moneyFormate = Integer.parseInt(money);
+        char[] data = new char[] { '零', '壹', '贰', '叁', '肆', '伍', '陆', '柒', '捌', '玖' };
+        char[] units = new char[] { '元', '拾', '佰', '仟', '万', '拾', '佰', '仟', '亿' };
+        StringBuffer sbf = new StringBuffer();
+        int unit = 0;
+        while (moneyFormate != 0) {
+            sbf.insert(0, units[unit++]);
+            int number = moneyFormate % 10;
+            sbf.insert(0, data[number]);
+            moneyFormate /= 10;
+        }
+        commonResponseBean.setCode(1);
+        commonResponseBean.setMsg(sbf.toString());
         return commonResponseBean;
     }
 
